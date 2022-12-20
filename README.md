f9cd15c5d2bc5e4b09b4fa5ed351af651375d696
Andrew Pettit <andrewp2010@gmail.com>
2022-12-20 14:59:46 -0800

    added GLFW submodule

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # OpenGLSandbox
 OpenGL support library and test framework
+
+Configure and build with CMake using cmake-gui or use comman line:
+Windows Batch Commands:
+```
+    ./configure.bat
+    ./build.bat
+    ./run.bat
+```
--- openGLCore/src/main.cpp
+++ openGLCore/src/main.cpp
@@ -1,6 +1,34 @@
 #include <iostream>
+#include <GLFW/glfw3.h>
 
 int main()
 {
 	std::cout << "Hellow CMake!" << std::endl;
+
+	GLFWwindow* window;
+	if (!glfwInit())
+	{
+		fprintf(stderr, "Failed to initialize GLFW\n");
+		exit(EXIT_FAILURE);
+	}
+	window = glfwCreateWindow(300, 200, "Gears", NULL, NULL);
+	if (!window)
+	{
+		fprintf(stderr, "Failed to open GLFW window\n");
+		glfwTerminate();
+		exit(EXIT_FAILURE);
+	}
+
+	// main loop
+	while (!glfwWindowShouldClose(window))
+	{
+		// swap buffers
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	// terminate GLFW
+	glfwTerminate();
+
+	return 0;
 }
